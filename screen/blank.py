ae9d0c0241342d5e6fd2ac66d924739e4fdbeee7
jennywalkr <84243404+jennywalkr@users.noreply.github.com>
2023-08-09 16:08:23 +0100

    Add files via upload

---
+++ screen/blank.py
@@ -0,0 +1,25 @@
+import os
+import sys 
+import time
+import logging
+import spidev as SPI
+sys.path.append("..")
+from lib import LCD_2inch
+from PIL import Image,ImageDraw,ImageFont
+
+# Raspberry Pi pin configuration:
+RST = 27
+DC = 25
+BL = 18
+bus = 0 
+device = 0 
+logging.basicConfig(level=logging.DEBUG)
+
+disp = LCD_2inch.LCD_2inch()
+disp.Init()
+disp.clear()
+image1 = Image.new("RGB", (disp.height, disp.width ), "BLACK")
+image1=image1.rotate(180)
+disp.ShowImage(image1)
+time.sleep(5)
+disp.module_exit()
