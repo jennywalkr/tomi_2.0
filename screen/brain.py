cb9d78f25e824992ac24299d6e11311bf1c1067d
jennywalkr <jenny.w5411@gmail.com>
2023-08-17 14:20:53 +0100

    dfghjk

--- screen/brain.py
+++ screen/brain.py
@@ -28,7 +28,7 @@ currentDate = currentDateAndTime.strftime("%d/%m/%y")
 
 # Fonts
 timeFont = ImageFont.truetype("../Font/RobotoMono-Regular.ttf",75)
-timerFont = ImageFont.truetype("../Font/RobotoMono-Regular.ttf",100)
+timerFont = ImageFont.truetype("../Font/RobotoMono-Regular.ttf",75)
 dateFont = ImageFont.truetype("../Font/RobotoMono-Light.ttf",30)
 RobotoMono38 = ImageFont.truetype("../Font/RobotoMono-Regular.ttf",38)
 RobotoMono28 = ImageFont.truetype("../Font/RobotoMono-Regular.ttf",28)
@@ -141,16 +141,18 @@ def disco():
 def countdown():
     mins = int(input("enter time in minutes: "))
     secs = mins*60
-    while secs > 0:
-        timer = str(timedelta(seconds = secs))
-        image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
-        draw = ImageDraw.Draw(image1)
-        draw.text((15, 50), timer, fill = "BLACK",font=timerFont)
-        image1=image1.rotate(180)
-        disp.ShowImage(image1)
-        print(timer)
-        time.sleep(1)
-        secs -= 1
+    schedule.every().seconds.do(timer)
+    def timer():
+        while secs > 0:
+            timer = str(timedelta(seconds = secs))
+            image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
+            draw = ImageDraw.Draw(image1)
+            draw.text((4, 65), timer, fill = "BLACK",font=timerFont)
+            image1=image1.rotate(180)
+            disp.ShowImage(image1)
+            print(timer)
+            time.sleep(1)
+            secs -= 1
  
 
 #def earth():
